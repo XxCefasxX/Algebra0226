0bcccbe6f65df3b1510668f6740faba7c59b3676
Pedro <salazar9_5@hotmail.com>
2026-02-05 12:09:12 -0600

    menu de opciones

--- Intro/Program.cs
+++ Intro/Program.cs
@@ -10,33 +10,31 @@ namespace Intro
     {
         static void Main(string[] args)
         {
-            int numero = 9;
-            string texto = "Pedro";
-            bool verdaderofalso = false;
-            double decimales = 10.0;
-           
-            
-            Console.WriteLine("Cambio realizados");
-            Console.WriteLine("Hola " + texto + " su numero es " + numero);
-            Console.WriteLine($"Hola {texto} su numero es {numero}");
 
-            string nombre = "";
-            int edad = 0;
-            string mayoria = "";
-            Console.WriteLine("Ingrese su nombre");
-            nombre = Console.ReadLine();
-            Console.WriteLine("Ingrese su edad");
-            edad = Convert.ToInt32(Console.ReadLine());
-            if (edad>17)
+            string opcion = "0";
+            Console.WriteLine("Que desea hacer?");
+            Console.WriteLine("1-Mostrar textos");
+            Console.WriteLine("2-Datos de persona");
+            Console.WriteLine("3-Calculadora");
+            opcion= Console.ReadLine();
+            switch (opcion)
             {
-                mayoria = "es mayor de edad";
+                case "1":
+                    MostrarTextos();
+                    break;
+                case "2":
+                    DatosDePersona();
+                    break;
+                case "3":
+                    Calculadora();
+                    break;
+                default:
+                    break;
             }
-            else
-            {
-                mayoria = "es menor de edad";
-            }
+        }
 
-            Console.WriteLine($"Bienvenido {nombre} su edad es {edad} por lo tanto {mayoria}");
+        protected static void Calculadora()
+        {
 
             int num1 = 0;
             int num2 = 0;
@@ -51,21 +49,68 @@ namespace Intro
             Console.WriteLine("Ingrese el segundo numero");
             num2 = Convert.ToInt32(Console.ReadLine());
             suma = Suma(num1, num2);
-            resta = num1 - num2;
-            mult = num1 * num2;
-            div = num1 / num2;
+            resta = Resta(num1, num2);
+            mult = Mult(num1, num2);
+            div = Div(num1, num2);
             Console.WriteLine($"La suma de {num1} + {num2} es: {suma}");
             Console.WriteLine($"La resta de {num1} - {num2} es: {resta}");
             Console.WriteLine($"La multi de {num1} * {num2} es: {mult}");
             Console.WriteLine($"La div de {num1} / {num2} es: {div}");
-
-           
         }
-
         protected static int Suma(int num1, int num2)
         {
             int resultado = num1 + num2;
            return resultado;
+        }
+        protected static int Resta(int num1, int num2)
+        {
+            int resultado = num1 - num2;
+            return resultado;
+        }
+        protected static int Mult(int num1, int num2)
+        {
+            int resultado = num1 * num2;
+            return resultado;
+        }
+        protected static int Div(int num1, int num2)
+        {
+            int resultado = num1 / num2;
+            return resultado;
+        }
+
+        protected static void MostrarTextos()
+        {
+            int numero = 9;
+            string texto = "Pedro";
+            bool verdaderofalso = false;
+            double decimales = 10.0;
+
+
+            Console.WriteLine("Cambio realizados");
+            Console.WriteLine("Hola " + texto + " su numero es " + numero);
+            Console.WriteLine($"Hola {texto} su numero es {numero}");
+        }
+
+        protected static void DatosDePersona()
+        {
+            string nombre = "";
+            int edad = 0;
+            string mayoria = "";
+            Console.WriteLine("Ingrese su nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+            if (edad > 17)
+            {
+                mayoria = "es mayor de edad";
+            }
+            else
+            {
+                mayoria = "es menor de edad";
+            }
+
+            Console.WriteLine($"Bienvenido {nombre} su edad es {edad} por lo tanto {mayoria}");
+
         }
     }
 }
